5ea40007207669ed82ac53938facb592773841a9
yangwenbang <1471275556@qq.com>
2017-12-02 14:48:15 +0800

    Signed-off-by: yangwenbang <1471275556@qq.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# ssm_dubbo
+
+dubbo 小Demo下载地址 https://github.com/yangwenbang/ssm_dubbo-master
+修改dubbo.properties中zookeeper地址 直接用tomcat启动即可
---
+++ dubbo-demo-consumer/src/main/java/cn/javaxxw/controller/UserController.java
@@ -0,0 +1,39 @@
+package cn.javaxxw.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.javaxxw.model.User;
+import cn.javaxxw.service.UserService;
+
+/**
+ * @author tuyong
+ * @version 1.0
+ * @desc
+ * @create 2017-11-29 15:38
+ **/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("register")
+    public Object register(String userName,String password,String nickName){
+        User user = userService.addUser(userName,password,nickName);
+        return user.toString();
+    }
+    
+    @GetMapping("findUser")
+    private List<User> findUser(){
+    	List<User> list = userService.findAllUsers();
+    	return list;
+    }
+
+}
