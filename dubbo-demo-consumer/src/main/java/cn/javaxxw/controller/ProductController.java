163ddd8c5bf5eb5c294a15613e5290fe0f213770
yangwenbang <1471275556@qq.com>
2018-02-02 22:38:08 +0800

    123

--- dubbo-demo-consumer/src/main/java/cn/javaxxw/controller/ProductController.java
+++ dubbo-demo-consumer/src/main/java/cn/javaxxw/controller/ProductController.java
@@ -32,7 +32,7 @@ public class ProductController {
         Product product = productService.addProduct(pname,price);
         return product.toString();
     }
-    //xxx111
+    //xxx123
     @GetMapping("findProduct")
     public ModelAndView findProduct(){
     	List<Product> list = productService.findAllProducts();
